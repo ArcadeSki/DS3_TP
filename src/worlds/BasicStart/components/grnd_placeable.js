4e5f5cfb5fe826bfc971a7eefc8cb9e8a8329f15
ArcadeSki <124928349+ArcadeSki@users.noreply.github.com>
2024-03-13 00:27:17 -0400

    i almost have placement working
     I got the player position, but its sending 2 arrays which is screwing with getting the real position.

--- src/worlds/BasicStart/components/grnd_placeable.js
+++ src/worlds/BasicStart/components/grnd_placeable.js
@@ -5,18 +5,18 @@ AFRAME.registerComponent('grnd-placeable', {
 
     },
     init:function(){
+        let greens = document.querySelectorAll('#tp');
         const CONTEXT_AF = this;
+        let playerPos = CIRCLES.getAvatarRigElement().object3D.position;
+
+        console.log('the player spawned at: ', playerPos);
+        console.log(greens[0].object3D.position);
 
         if (CONTEXT_AF.el.hasAttribute('circles-pickup-object') === false) {
             CONTEXT_AF.el.setAttribute('circles-pickup-object', {});
         }
 
-        play = CIRCLES.getAvatarElement();
-
-        if(play.object3D.position < 1000)
-        {
-            CONTEXT_AF.el.setAttribute('circles-pickup-object', {dropPosition:'0 0.1 0', dropRotation:'0 0.1 0'});
-        }
+        CONTEXT_AF.el.setAttribute('circles-pickup-object', {dropRotation:'0 0.1 0'});
         
     }
 });
