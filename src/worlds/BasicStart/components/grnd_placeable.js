b9620d63e567462840e23256ed6fab32b8714d37
ArcadeSki <124928349+ArcadeSki@users.noreply.github.com>
2024-03-21 17:37:03 -0400

    filler items fully set
    all we have to do now is get the real ones in... i hope

--- src/worlds/BasicStart/components/grnd_placeable.js
+++ src/worlds/BasicStart/components/grnd_placeable.js
@@ -3,8 +3,7 @@
 //WHEN PICKED UP, THE OBJECT WILL FOLLOW YOU. WHEN RELEASED, TI WILL EITHER GO BACK TO ITS INITAL POSITION OR TO THE TP'S CORRESPONDING PLACEMAT.
 AFRAME.registerComponent('grnd-placeable', {
     schema: {
-        initialPos:{type:'array'},
-        tester:{type:'int', default:0}
+        initialPos:{type:'string'}
     },
     init:function(){
         const CONTEXT_AF = this;
@@ -19,12 +18,12 @@ AFRAME.registerComponent('grnd-placeable', {
         CONTEXT_AF.data.initialPos = String(CONTEXT_AF.el.object3D.position.x) + ' ' + String(CONTEXT_AF.el.object3D.position.y) + ' ' + String(CONTEXT_AF.el.object3D.position.z);
         console.log("initial pos is: ", CONTEXT_AF.data.initialPos);
 
-        if (CONTEXT_AF.el.hasAttribute('circles-pickup-object') === false) {
-            CONTEXT_AF.el.setAttribute('circles-pickup-object', {});
+        if (CONTEXT_AF.el.hasAttribute('circles-artefact') === false) {
+            CONTEXT_AF.el.setAttribute('circles-artefact', {});
         }
 
         //make sure object is flat when released
-        CONTEXT_AF.el.setAttribute('circles-pickup-object', {dropRotation:'0 0.1 0'});
+        CONTEXT_AF.el.setAttribute('circles-artefact', {origRotation:'0 0.1 0'});
         
         //tick:function was having issues getting variables
         setInterval( () => {
@@ -33,18 +32,18 @@ AFRAME.registerComponent('grnd-placeable', {
             //if user is on a teleport point, release sends the object to the corresponding position
             if( (playerPos.x === greens[0].object3D.position.x) &&
                 (playerPos.z === greens[0].object3D.position.z))
-                { CONTEXT_AF.el.setAttribute('circles-pickup-object', {dropPosition: mats[0].object3D.position}); } //object goes on red mat
+                { CONTEXT_AF.el.setAttribute('circles-artefact', {origPosition: mats[0].object3D.position}); } //object goes on red mat
 
             else if((playerPos.x === greens[1].object3D.position.x) &&
                     (playerPos.z === greens[1].object3D.position.z))
-            { CONTEXT_AF.el.setAttribute('circles-pickup-object', {dropPosition: mats[1].object3D.position}); } //object goes on green mat
-
+            { CONTEXT_AF.el.setAttribute('circles-artefact', {origPosition: mats[1].object3D.position}); } //object goes on green mat
+/*
             else if((playerPos.x === greens[2].object3D.position.x) &&
                     (playerPos.z === greens[2].object3D.position.z))
-            { CONTEXT_AF.el.setAttribute('circles-pickup-object', {dropPosition: mats[2].object3D.position}); } //object goes on green mat
+            { CONTEXT_AF.el.setAttribute('circles-artefact', {origPosition: mats[2].object3D.position}); } //we have two objects in the scene right now*/
 
             //if user is not on a teleport point, release sends the object to its inital position
-            else{ CONTEXT_AF.el.setAttribute('circles-pickup-object', {dropPosition: CONTEXT_AF.data.initialPos}); }
+            else{ CONTEXT_AF.el.setAttribute('circles-artefact', {origPosition: CONTEXT_AF.data.initialPos}); }
         }, 10);
     }
 });
