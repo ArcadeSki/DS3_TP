4c8b0e7cb2d01dd53544d9a0a3c4f4bcd31b483a
ArcadeSki <124928349+ArcadeSki@users.noreply.github.com>
2024-04-05 16:07:59 -0400

    complete room to unlock portal

--- src/worlds/BasicStart/components/checkLayout.js
+++ src/worlds/BasicStart/components/checkLayout.js
@@ -3,8 +3,10 @@
 function check_ingenium(){
 
     //these can be changed for each room
-    let obj1 = document.querySelectorAll('.Artefact-Headphones'); //green
-    let obj2 = document.querySelectorAll('.Artefact-Mailbox'); //red
+    let obj1 = document.querySelectorAll('.cube'); //green
+    let obj2 = document.querySelectorAll('.box'); //red
+    let obj3 = document.querySelectorAll('.de'); //red
+
 
     console.log('obj1: ', (obj1[0].getAttribute('position').x).toFixed(2), (obj1[1].getAttribute('position').x).toFixed(2));
     console.log('obj1: ', (obj1[0].getAttribute('position').y).toFixed(2), (obj1[1].getAttribute('position').y).toFixed(2));
@@ -30,6 +32,15 @@ function check_ingenium(){
                     (obj3[0].getAttribute('position').z === obj3[1].getAttribute('position').z))
                     {
                         console.log('Ingenium set is looking good');
+
+                        let complete = document.createElement('a-entity');
+
+                        complete.setAttribute('position','-5 1.5 4');
+                        complete.setAttribute('rotation','0 90 0');
+                        complete.setAttribute('circles-portal','title_text:Quebec; link_url:/w/WIT_1_Kitchen');
+
+                        document.querySelector('a-scene').appendChild(complete);
+
                     }
                 else{
                     console.log('WRONG');
@@ -70,6 +81,14 @@ function check_quebec(){
             ((obj2[0].getAttribute('position').z).toFixed(2) === (obj2[1].getAttribute('position').z).toFixed(2)))
             {
                 console.log('Quebec set is looking good');
+
+                let complete = document.createElement('a-entity');
+
+                        complete.setAttribute('position','-4.8 1.5 3.8');
+                        complete.setAttribute('rotation','0 0 0');
+                        complete.setAttribute('circles-portal','title_text:Lab; link_url:/w/BasicStart');
+
+                        document.querySelector('a-scene').appendChild(complete);
             }
         else{
                 console.log('WRONG');
